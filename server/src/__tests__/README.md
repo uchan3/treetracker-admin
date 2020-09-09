0cc617d20f2b670e27c501e223a409e021a5700a
uchan3 <urey.chan3@gmail.com>
2020-09-09 10:08:31 -0600

    Continue testing workflow

--- server/src/__tests__/README.md
+++ server/src/__tests__/README.md
@@ -1,3 +1,4 @@
 # Tests
 
 Please place your tests in this folder.
+Note that there will be two test folders, one for unit and another for integration.
--- server/src/__tests__/species.controller.ts
+++ server/src/__tests__/species.controller.ts
@@ -4,6 +4,7 @@ import { SpeciesController } from '../controllers';
 import { SpeciesRepository } from '../repositories';
 require('dotenv').config();
 
+// Sample test
 /*Purpose: Include env. variables in test in order to 
 map to env. variables in Github Actions.*/
 describe('Species Controller Integration Tests', () => {
